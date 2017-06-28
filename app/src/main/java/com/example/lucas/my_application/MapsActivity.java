9890888e30ad00dabc81162ade2741cfc091c84a
Augusto <gstfrr@gmail.com>
2017-06-28 14:18:47 -0300

    Consertar TCP

--- app/src/main/java/com/example/lucas/my_application/MapsActivity.java
+++ app/src/main/java/com/example/lucas/my_application/MapsActivity.java
@@ -11,6 +11,13 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -63,5 +70,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mamute));
 
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        String serverHostname = new String ("127.0.0.1");
+
+        System.out.println ("Attemping to connect to host " +
+                serverHostname + " on port 10008.");
+
+        Socket echoSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            echoSocket = new Socket(serverHostname, 10008);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                    echoSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: " + serverHostname);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for "
+                    + "the connection to: " + serverHostname);
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String userInput;
+
+        System.out.println ("Type Message (\"Bye.\" to quit)");
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+            if (userInput.equals("Bye."))
+                break;
+            System.out.println("echo: " + in.readLine());
+        }
+
+        out.close();
+        in.close();
+        stdIn.close();
+        echoSocket.close();
     }
 }
