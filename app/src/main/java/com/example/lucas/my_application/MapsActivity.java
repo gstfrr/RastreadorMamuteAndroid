3b72d656eb040c06fe3182b50a179d5e2508c738
Augusto <gstfrr@gmail.com>
2017-06-28 14:08:48 -0300

    Remoção dos testes Interface do Mapa

--- app/src/main/java/com/example/lucas/my_application/MapsActivity.java
+++ app/src/main/java/com/example/lucas/my_application/MapsActivity.java
@@ -1,113 +1,28 @@
 package com.example.lucas.my_application;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(
-                        new String[]{
-                                Manifest.permission.ACCESS_COARSE_LOCATION,
-                                Manifest.permission.ACCESS_FINE_LOCATION,
-                                Manifest.permission.INTERNET}
-                        ,10);
-            }
-            return;
-        }
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-
-
-
-
-        locationManager.requestLocationUpdates("gps", 1000, 0, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-                Double Latitude = location.getLatitude();
-                Double Longitude = location.getLongitude();
-
-                String locus;
-                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-                locus = ( sdf.format(new Date())+" "+Latitude+" "+Longitude);
-
-                try {
-                    udpmsg(locus);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                LatLng mamute = new LatLng(Latitude, Longitude);
-                mMap.addMarker(new MarkerOptions().position(mamute).title("Mamute aqui"));
-                CameraUpdate cam = CameraUpdateFactory.newLatLng(mamute);
-                //CameraUpdateFactory.zoomTo(30);
-                mMap.moveCamera(cam);
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-            }
-        });
-
-
     }
 
 
@@ -124,18 +39,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-    }
 
-    public void udpmsg(String text) throws IOException {
-        String address = "177.105.60.245";
-        int port=7566;
+        LatLng cantina = new LatLng(-21.2273078, -44.9775676);
+        MarkerOptions marcadorCatina = new MarkerOptions()
+                .position(cantina)
+                .title("Cantina Central")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        mMap.addMarker(marcadorCatina);
 
-        InetAddress host = InetAddress.getByName(address);
+        LatLng portaria = new LatLng(-21.2310722, -44.9940471);
+        MarkerOptions marcadorPortaria = new MarkerOptions()
+                .position(portaria)
+                .title("Portaria")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));;
+        mMap.addMarker(marcadorPortaria);
 
-        byte[] data = text.getBytes();
-        DatagramPacket pac = new DatagramPacket(data, data.length, host, port);
-        DatagramSocket soc = new DatagramSocket();
-        soc.send(pac);
+        LatLng mamute = new LatLng(-34, 151);
+        MarkerOptions marcadorMamute = new MarkerOptions()
+                .position(portaria)
+                .title("Mamute está aqui")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));;
+        mMap.addMarker(marcadorMamute);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mamute));
 
     }
 }
